8701ef8f816cf071de97196bad95eb53b351caec
bfren <ben@bcgdesign.com>
2022-02-11 12:26:30 +0000

    v1.3.7 (#14)
    Minor updates
    * Using latest base images
    
    Build updates
    * Using Dockerfile generator
    * Using bf-echo in install
    
    Environment updates
    * Adding DS_Store to gitignore

---
+++ generate-dockerfiles.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -euo pipefail
+
+docker pull bfren/alpine
+
+BASE_REVISION="1.4.10"
+echo "Base: ${BASE_REVISION}"
+
+PHP_VERSIONS="7.3 7.4 7.4-edge 8.0 8.0-edge"
+for V in ${PHP_VERSIONS} ; do
+
+    echo "PHP ${V}"
+    PHP_MAJOR="$(echo ${V} | cut -c 1)"
+    ALPINE_MINOR=`cat ./${V}/ALPINE_MINOR`
+
+    DOCKERFILE=$(docker run \
+        -v ${PWD}:/ws \
+        -e BF_DEBUG=0 \
+        bfren/alpine esh \
+        "/ws/Dockerfile.esh" \
+        BASE_REVISION=${BASE_REVISION} \
+        ALPINE_MINOR=${ALPINE_MINOR} \
+        PHP_MAJOR=${PHP_MAJOR} \
+        PHP_MINOR=${V}
+    )
+
+    echo "${DOCKERFILE}" > ./${V}/Dockerfile
+
+done
+
+docker system prune -f
+echo "Done."
